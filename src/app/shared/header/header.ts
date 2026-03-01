9b6f68e54145f444b7e0fc0dc3f97a6ac452d09b
Vlciss <adrianvlcko69@gmail.com>
2026-03-01 12:09:58 +0100

    Remove header scroll animation

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -8,23 +8,5 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './header.scss'
 })
 export class Header {
-  isHeaderVisible = true;
-  private lastScrollY = 0;
 
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const currentScrollY = window.scrollY;
-    const scrollDelta = currentScrollY - this.lastScrollY;
-
-    // Hide header when scrolling down more than 10px
-    if (scrollDelta > 10 && currentScrollY > 100) {
-      this.isHeaderVisible = false;
-    } 
-    // Show header when scrolling up more than 10px
-    else if (scrollDelta < -10) {
-      this.isHeaderVisible = true;
-    }
-
-    this.lastScrollY = currentScrollY;
-  }
 }
