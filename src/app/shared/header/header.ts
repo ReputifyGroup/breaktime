32c40b2603d5a22f867fb18c5e136420df11f95b
Vlciss <adrianvlcko69@gmail.com>
2026-03-01 12:03:47 +0100

    Add hide/show header animation on scroll

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -7,6 +7,33 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
-export class Header {
+export class Header implements OnInit, OnDestroy {
+  isHeaderVisible = true;
+  private lastScrollY = 0;
+  private scrollListener: (() => void) | null = null;
 
+  ngOnInit() {
+    this.scrollListener = this.onScroll.bind(this);
+    window.addEventListener('scroll', this.scrollListener);
+  }
+
+  ngOnDestroy() {
+    if (this.scrollListener) {
+      window.removeEventListener('scroll', this.scrollListener);
+    }
+  }
+
+  private onScroll() {
+    const currentScrollY = window.scrollY;
+    const scrollDelta = currentScrollY - this.lastScrollY;
+
+    // Hide header when scrolling down (delta > 0), show when scrolling up (delta < 0)
+    if (scrollDelta > 10) {
+      this.isHeaderVisible = false;
+    } else if (scrollDelta < -10) {
+      this.isHeaderVisible = true;
+    }
+
+    this.lastScrollY = currentScrollY;
+  }
 }
